77b9945bc8f8fa3ff5b66b2fcdc46f0ce742826c
Plane-Jumper5 <romariobelyaev@gmail.com>
2025-03-04 19:33:40 +0200

    5

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,14 +1,13 @@
 
 
-const Feedback = ({ feedback, total, positive }) => {
+const Feedback = ({ feedback, total, positive, neutral, negative }) => {
   return (
-    <div>
+    <div className="feedback-container">
       <h2>Feedback Summary</h2>
-      <p>Good: {feedback.good}</p>
-      <p>Neutral: {feedback.neutral}</p>
-      <p>Bad: {feedback.bad}</p>
-      <p>Total feedbacks: {total}</p>
-      <p>Positive feedback: {positive}%</p>
+      <p>Good: {feedback.good} {positive}</p>
+      <p>Neutral: {feedback.neutral} {neutral}</p>
+      <p>Bad: {feedback.bad} {negative}</p>
+      <p>Total: {total}%</p>
     </div>
   );
 };
