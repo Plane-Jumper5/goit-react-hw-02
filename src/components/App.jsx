ea5f75dfee73ce4353f246ccb47462f3234d7ce3
Plane-Jumper5 <romariobelyaev@gmail.com>
2025-03-04 16:04:40 +0200

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,4 @@
-import userData from "./userData.json";
-import Profile from "./Profile";
-import FriendList from "./FriendList";
-import friends from "./friends.json";
-import TransactionHistory from "./TransactionHistory";
-import transactions from "./transactions.json";
 
-const App = () => {
-  return (
-    <div>
-    <Profile
-      name={userData.username}
-      tag={userData.tag}
-      location={userData.location}
-      image={userData.avatar}
-      stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-       <TransactionHistory items={transactions} />
-
-    </div>
-  );
-};
-
-export default App;
 
 
 
